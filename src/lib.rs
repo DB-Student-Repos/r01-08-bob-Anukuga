05c5aa5f8b9640ccabab1474bab9d5899aaf921b
anukuga <anukuga02@gmail.com>
2024-03-14 15:17:07 +0100

    passed

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,13 @@
 pub fn reply(message: &str) -> &str {
-    unimplemented!("have Bob reply to the incoming message: {message}")
+    let message = message.trim();
+    if message.is_empty() {
+        "Fine. Be that way!"
+    } else if message.ends_with('?') {
+        "Sure."
+    } else if message.to_uppercase() == message {
+        "Whoa, chill out!"
+    } else {
+        "Whatever."
+    }
 }
+
